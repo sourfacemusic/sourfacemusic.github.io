15769ba4851f3b675d7c3d029b2a5ec993220382
sourfacemusic <sourfacemusic@gmail.com>
2026-07-20 15:33:26 -0400

    Add central SOURFACEMUSIC site configuration

---
+++ site-config.js
@@ -0,0 +1,25 @@
+window.SFM_CONFIG = Object.freeze({
+  brand: 'SOURFACEMUSIC',
+  shortName: 'S.F.M.',
+  tagline: 'Music production, DJ culture, artist development and youth education.',
+  shortBio: 'Independent music production, remix blends, DJ culture, artist development and youth education from New York.',
+  website: 'https://sourfacemusic.github.io/',
+  linksPage: 'https://sourfacemusic.github.io/links.html',
+  fundraiserPage: 'https://sourfacemusic.github.io/support.html',
+  email: 'sourfacemusic@gmail.com',
+  phoneDisplay: '(646) 286-7210',
+  phoneHref: '+16462867210',
+  location: 'New York, NY',
+  artwork: {
+    banner: 'assets/sfm-cosmic-banner.svg',
+    square: 'assets/sfm-cosmic-square.svg',
+    story: 'assets/sfm-cosmic-story.svg'
+  },
+  socials: {
+    youtube: 'https://www.youtube.com/@SOURFACEMUSIC4LIFE',
+    tiktok: 'https://www.tiktok.com/@sourfacemusic',
+    soundcloud: 'https://soundcloud.com/sourfacemusic/',
+    facebook: 'https://www.facebook.com/sfacemusic/',
+    vimeo: 'https://vimeo.com/sourfacemusic/'
+  }
+});
